6028b3eb67b5a70fe4e0a1ceb001403200a2478d
Samir-43ru45 <freedomsamir2005@gmail.com>
2022-10-19 10:39:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+function PortfolioPage(props){
+  return(
+    <>
+      <h1>
+        {props.title}
+      </h1>
+    </>
+  )
+}
+function Title(){
+  return(
+    <>
+    <Title title="Дастан красавчик"/>  
+    </>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PortfolioPage/>
+      <Title/>
     </div>
   );
 }
-
 export default App;
